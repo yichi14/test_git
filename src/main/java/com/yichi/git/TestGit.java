cecb362e64ad139e9e06610526e6fbb60c740eba
yichi <svip996@vip.qq.com>
2021-07-30 15:45:12 +0800

    first commit1

---
+++ src/main/java/com/yichi/git/TestGit.java
@@ -0,0 +1,16 @@
+package com.yichi.git;
+
+/**
+ * @author xc
+ */
+public class TestGit {
+
+
+    public static void main(String[] args) {
+        System.out.println("TestGIt------");
+    }
+
+}
+
+
+
