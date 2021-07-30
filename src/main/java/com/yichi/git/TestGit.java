2adae2b5e7446643a3f8786575ed67fd8965c720
yichi <svip996@vip.qq.com>
2021-07-30 15:55:39 +0800

    third commit1

--- src/main/java/com/yichi/git/TestGit.java
+++ src/main/java/com/yichi/git/TestGit.java
@@ -9,6 +9,7 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("TestGIt------");
         System.out.println("TestGIt2------");
+        System.out.println("TestGIt3------");
     }
 
 }
