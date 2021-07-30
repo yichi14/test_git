5e4aad3feddd59e2611be6a5c551148b09e56e1f
yichi14 <85943591+yichi14@users.noreply.github.com>
2021-07-31 00:23:34 +0800

    Update TestGit2.java

--- src/main/java/com/yichi/git/TestGit2.java
+++ src/main/java/com/yichi/git/TestGit2.java
@@ -8,6 +8,7 @@ public class TestGit2 {
 
     public static void main(String[] args) {
         System.out.println("绅士-------TestGit");
+        System.out.println("你退半步的动作认真的吗-------TestGit");
     }
 
 
